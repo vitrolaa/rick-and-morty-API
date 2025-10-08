b318d688941225d965eb2fa7ef3ec5a7ec18759c
Vitor Brito de Bastos <130709723+vitrolaa@users.noreply.github.com>
2025-10-08 18:01:19 -0300

    08-10-25

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Index from './src/app/index'
+
+export default function App() {
+	return <Index />
+}
